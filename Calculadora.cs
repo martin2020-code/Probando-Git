2128b37280e8bff4a6d724c279648730e682b78f
martin2020-code <82909403+martin2020-code@users.noreply.github.com>
2021-12-18 00:29:24 -0500

    Pontencia y raiz

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Probando_Git
 {
     public class Calculadora
@@ -6,5 +8,7 @@ namespace Probando_Git
         float Substract (int x, int y) => x - y;
         float Multiply (int x, int y) => x * y;
         float Divide (int x, int y) => x / y;
+        double Power (double x, double y) => Math.Pow (x, y); 
+        double Square (double x) => Math.Pow(x, 0.5);
     }
 }
