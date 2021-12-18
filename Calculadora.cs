13201899e42b4abf1eed7157a949f5b7406e4e27
martin2020-code <82909403+martin2020-code@users.noreply.github.com>
2021-12-18 00:25:51 -0500

    Operaciones basicas

---
+++ Calculadora.cs
@@ -0,0 +1,10 @@
+namespace Probando_Git
+{
+    public class Calculadora
+    {
+        float Add (int x, int y) => x + y;
+        float Substract (int x, int y) => x - y;
+        float Multiply (int x, int y) => x * y;
+        float Divide (int x, int y) => x / y;
+    }
+}
